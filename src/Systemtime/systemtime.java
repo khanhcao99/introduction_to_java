53141d0053f3f0906cd0dda843c913dc758df76d
khanhcao99 <khanhboy1324@gmail.com>
2022-06-02 14:36:46 +0700

    Systemtime

---
+++ src/Systemtime/systemtime.java
@@ -0,0 +1,11 @@
+package Systemtime;
+
+import java.util.Date;
+
+public class systemtime {
+    public static void main(String[] args) {
+        Date now = new Date();
+//        Lấy ra thời gian hiện tại
+        System.out.println("thời gian hiện tại là: " + now);
+    }
+}
